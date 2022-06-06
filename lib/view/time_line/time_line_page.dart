655112f1286b47830f5b87959e9a5fc774ad8412
yuki <yuki.onishi1129@gmail.com>
2022-06-06 15:39:45 +0900

    TimeLineページのUI作成

--- lib/view/time_line/time_line_page.dart
+++ lib/view/time_line/time_line_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../model/account.dart';
+import '../../model/post.dart';
 
 class TimeLinePage extends StatefulWidget {
   const TimeLinePage({Key? key}) : super(key: key);
@@ -22,12 +24,92 @@ class _TimeLinePageState extends State<TimeLinePage> {
     updatedTime: DateTime.now(),
   );
 
+  List<Post> postList = [
+    Post(
+      id: '1',
+      content: '初めまして',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    ),
+    Post(
+      id: '2',
+      content: '初めまして2回目',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('タイムライン'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'タイムライン',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Theme.of(context).canvasColor,
+          elevation: 1,
+        ),
+        body: ListView.builder(
+          itemCount: postList.length,
+          itemBuilder: (context, index) {
+            return Container(
+              decoration: BoxDecoration(
+                  border: index == 0
+                      ? const Border(
+                          top: BorderSide(color: Colors.grey, width: 0),
+                          bottom: BorderSide(color: Colors.grey, width: 0),
+                        )
+                      : const Border(
+                          bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0,
+                        ))),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 15,
+              ),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 22,
+                    foregroundImage: NetworkImage(myAccount.imagePath),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    myAccount.name,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    '@${myAccount.userId}',
+                                    style: const TextStyle(color: Colors.grey),
+                                  ),
+                                ],
+                              ),
+                              Text(DateFormat('M/d/yy')
+                                  .format(postList[index].createdTime!))
+                            ],
+                          ),
+                          Text(postList[index].content)
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+        ));
   }
 }
