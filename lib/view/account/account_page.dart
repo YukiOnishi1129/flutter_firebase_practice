ebfe1a37bb67fc813919f5ce3f16d5273b3bac6a
yuki <yuki.onishi1129@gmail.com>
2022-06-09 00:36:32 +0900

    AccountPageのUI作成

--- lib/view/account/account_page.dart
+++ lib/view/account/account_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../model/account.dart';
+import '../../model/post.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({Key? key}) : super(key: key);
@@ -8,8 +12,169 @@ class AccountPage extends StatefulWidget {
 }
 
 class _AccountPageState extends State<AccountPage> {
+  //インスタンス作成
+  Account myAccount = Account(
+    id: '1',
+    name: 'Flutterラボ',
+    selfIntroduction: 'こんばんは',
+    userId: 'flutter_labo',
+    imagePath:
+        'https://yt3.ggpht.com/ngVd2-zv5o3pGUCfiVdZXCHhnq_g1Lo1Y8DbrmB9O8G7DG0IWUQJgsacqsI_LRvZE8JTsbQIuQ=s900-c-k-c0x00ffffff-no-rj',
+    createdTime: DateTime.now(),
+    updatedTime: DateTime.now(),
+  );
+
+  List<Post> postList = [
+    Post(
+      id: '1',
+      content: '初めまして',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    ),
+    Post(
+      id: '2',
+      content: '初めまして2回目',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(right: 15, left: 15, top: 20),
+                  height: 200,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 32,
+                                foregroundImage:
+                                    NetworkImage(myAccount.imagePath),
+                              ),
+                              const SizedBox(width: 10),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    myAccount.name,
+                                    style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text('@${myAccount.userId}',
+                                      style: TextStyle(color: Colors.grey)),
+                                ],
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 15),
+                          OutlinedButton(
+                              onPressed: () {}, child: const Text('編集'))
+                        ],
+                      ),
+                      Text(myAccount.selfIntroduction)
+                    ],
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: Colors.blue, width: 3),
+                    ),
+                  ),
+                  child: const Text(
+                    '投稿',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(
+                    child: ListView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: postList.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                          border: index == 0
+                              ? const Border(
+                                  top: BorderSide(color: Colors.grey, width: 0),
+                                  bottom:
+                                      BorderSide(color: Colors.grey, width: 0),
+                                )
+                              : const Border(
+                                  bottom: BorderSide(
+                                  color: Colors.grey,
+                                  width: 0,
+                                ))),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 15,
+                      ),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 22,
+                            foregroundImage: NetworkImage(myAccount.imagePath),
+                          ),
+                          Expanded(
+                            child: Container(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Text(
+                                            myAccount.name,
+                                            style: const TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          Text(
+                                            '@${myAccount.userId}',
+                                            style: const TextStyle(
+                                                color: Colors.grey),
+                                          ),
+                                        ],
+                                      ),
+                                      Text(DateFormat('M/d/yy')
+                                          .format(postList[index].createdTime!))
+                                    ],
+                                  ),
+                                  Text(postList[index].content)
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
