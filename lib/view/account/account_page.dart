8d866e0b25c7aecc0c882d09524bdd905f54ae28
yuki <yuki.onishi1129@gmail.com>
2022-06-14 17:26:17 +0900

    自分の投稿をアカウントページに表示

--- lib/view/account/account_page.dart
+++ lib/view/account/account_page.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firevase_practice/utils/authentication.dart';
+import 'package:flutter_firevase_practice/utils/firestore/posts.dart';
+import 'package:flutter_firevase_practice/utils/firestore/users.dart';
 import 'package:intl/intl.dart';
 
 import '../../model/account.dart';
@@ -17,21 +19,6 @@ class AccountPage extends StatefulWidget {
 class _AccountPageState extends State<AccountPage> {
   //インスタンス作成
   Account? myAccount = Authentication.myAccount!;
-
-  List<Post> postList = [
-    Post(
-      id: '1',
-      content: '初めまして',
-      postAccountId: '1',
-      createdTime: Timestamp.now(),
-    ),
-    Post(
-      id: '2',
-      content: '初めまして2回目',
-      postAccountId: '1',
-      createdTime: Timestamp.now(),
-    )
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -112,73 +99,110 @@ class _AccountPageState extends State<AccountPage> {
                   ),
                 ),
                 Expanded(
-                    child: ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: postList.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      decoration: BoxDecoration(
-                          border: index == 0
-                              ? const Border(
-                                  top: BorderSide(color: Colors.grey, width: 0),
-                                  bottom:
-                                      BorderSide(color: Colors.grey, width: 0),
-                                )
-                              : const Border(
-                                  bottom: BorderSide(
-                                  color: Colors.grey,
-                                  width: 0,
-                                ))),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 10,
-                        vertical: 15,
-                      ),
-                      child: Row(
-                        children: [
-                          CircleAvatar(
-                            radius: 22,
-                            foregroundImage:
-                                NetworkImage('${myAccount?.imagePath}'),
-                          ),
-                          Expanded(
-                            child: Container(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Text(
-                                            '${myAccount?.name}',
-                                            style: const TextStyle(
-                                                fontWeight: FontWeight.bold),
+                    child: StreamBuilder<QuerySnapshot>(
+                        stream: UserFirestore.users
+                            .doc(myAccount!.id)
+                            .collection('my_posts')
+                            .orderBy('created_time', descending: true)
+                            .snapshots(),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            List<String> myPostIds = List.generate(
+                                snapshot.data!.docs.length, (index) {
+                              return snapshot.data!.docs[index].id;
+                            });
+                            return FutureBuilder<List<Post>?>(
+                                future:
+                                    PostFirestore.getPostsFromIds(myPostIds),
+                                builder: (context, snapshot) {
+                                  if (snapshot.hasData) {
+                                    return ListView.builder(
+                                      physics:
+                                          const NeverScrollableScrollPhysics(),
+                                      itemCount: snapshot.data!.length,
+                                      itemBuilder: (context, index) {
+                                        Post post = snapshot.data![index];
+                                        return Container(
+                                          decoration: BoxDecoration(
+                                              border: index == 0
+                                                  ? const Border(
+                                                      top: BorderSide(
+                                                          color: Colors.grey,
+                                                          width: 0),
+                                                      bottom: BorderSide(
+                                                          color: Colors.grey,
+                                                          width: 0),
+                                                    )
+                                                  : const Border(
+                                                      bottom: BorderSide(
+                                                      color: Colors.grey,
+                                                      width: 0,
+                                                    ))),
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 10,
+                                            vertical: 15,
                                           ),
-                                          Text(
-                                            '@${myAccount?.userId}',
-                                            style: const TextStyle(
-                                                color: Colors.grey),
+                                          child: Row(
+                                            children: [
+                                              CircleAvatar(
+                                                radius: 22,
+                                                foregroundImage: NetworkImage(
+                                                    '${myAccount?.imagePath}'),
+                                              ),
+                                              Expanded(
+                                                child: Container(
+                                                  child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      Row(
+                                                        mainAxisAlignment:
+                                                            MainAxisAlignment
+                                                                .spaceBetween,
+                                                        children: [
+                                                          Row(
+                                                            children: [
+                                                              Text(
+                                                                '${myAccount?.name}',
+                                                                style: const TextStyle(
+                                                                    fontWeight:
+                                                                        FontWeight
+                                                                            .bold),
+                                                              ),
+                                                              Text(
+                                                                '@${myAccount?.userId}',
+                                                                style: const TextStyle(
+                                                                    color: Colors
+                                                                        .grey),
+                                                              ),
+                                                            ],
+                                                          ),
+                                                          Text(DateFormat(
+                                                                  'M/d/yy')
+                                                              .format(post
+                                                                  .createdTime!
+                                                                  .toDate()))
+                                                        ],
+                                                      ),
+                                                      Text(post.content)
+                                                    ],
+                                                  ),
+                                                ),
+                                              )
+                                            ],
                                           ),
-                                        ],
-                                      ),
-                                      Text(DateFormat('M/d/yy').format(
-                                          postList[index]
-                                              .createdTime!
-                                              .toDate()))
-                                    ],
-                                  ),
-                                  Text(postList[index].content)
-                                ],
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                ))
+                                        );
+                                      },
+                                    );
+                                  } else {
+                                    return Container();
+                                  }
+                                });
+                          } else {
+                            return Container();
+                          }
+                        }))
               ],
             ),
           ),
